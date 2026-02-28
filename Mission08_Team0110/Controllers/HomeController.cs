0e1120f5c562bd8e56d103829b5f6379f3e7aaeb
jsorganamerica <jsorganamerica@gmail.com>
2026-02-27 19:31:45 -0700

    Added Quadrants view and stub controller action

--- Mission08_Team0110/Controllers/HomeController.cs
+++ Mission08_Team0110/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 // using Mission08_Team0110.Models;
-
+using Mission08_Team0110.Models;
 namespace Mission08_Team0110.Controllers;
 
 public class HomeController : Controller
@@ -14,5 +14,9 @@ public class HomeController : Controller
     public IActionResult Create()
     {
         return View();
+    }
+    public IActionResult Quadrants()
+    {
+        return View(new List<TaskItem>());
     }
 }
